154e16d139cd0d62c4551b999918ac9612c73f77
zjefersound <jeferson.2017317264@gmail.com>
2020-10-23 17:27:02 -0300

    mongoose add

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,18 @@
-import express from 'express';
-require('dotenv').config();
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
+dotenv.config();
 const app = express();
-
 const port = process.env.PORT || 3333;
+const mongoUri = process.env.MONGO_URI || '';
 
-app.listen(port, () => console.log(`Backend is running at ${port}`))
+mongoose
+  .connect(mongoUri, { useNewUrlParser: true })
+  .then(() => console.log("DB Connected"));
+
+mongoose.connection.on("error", (err) => {
+  console.log(`DB connection error: ${err.message}`);
+});
+
+app.listen(port, () => console.log(`Backend is running at ${port}`));
