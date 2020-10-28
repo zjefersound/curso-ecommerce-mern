d2d9d2f51ede77238dfb2925c66d066f11e05a5e
zjefersound <jeferson.2017317264@gmail.com>
2020-10-28 14:55:24 -0300

    bcrypt comapre

--- backend/src/app/controllers/UserController.ts
+++ backend/src/app/controllers/UserController.ts
@@ -30,33 +30,35 @@ class UserController {
     const { email, password } = req.body;
 
     User.findOne({ email }, (err, user) => {
-      if (err || !user) {
-        return res.status(400).json({
-          error: "An user with that password does not exists.",
-        });
-      }
-      const isValidPassword = bcrypt.compare(password, user.hashed_password);
-
-      if (!isValidPassword) {
-        return res.sendStatus(401).json({
-          error: "Email and password don't match",
-        });
-      }
-      const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET || '');
-
-      const { _id, name, email, role } = user;
-
-      const tokenExpiresIn = new Date();
-      tokenExpiresIn.setMonth(tokenExpiresIn.getMonth() + 3);
-
-      return res
-        .cookie("t", token, { expires: tokenExpiresIn })
-        .json({
-          user: { _id, name, email, role },
-          token,
-        });
+      (async () => {
+        if (err || !user) {
+          return res.status(400).json({
+            error: "An user with that password does not exists.",
+          });
+        }
+        const isValidPassword = await bcrypt.compare(password, user.hashed_password);
+  
+        if (!isValidPassword) {
+          return res.sendStatus(401).json({
+            error: "Email and password don't match",
+          });
+        }
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET || '');
+  
+        const { _id, name, email, role } = user;
+  
+        const tokenExpiresIn = new Date();
+        tokenExpiresIn.setMonth(tokenExpiresIn.getMonth() + 3);
+  
+        return res
+          .cookie("t", token, { expires: tokenExpiresIn })
+          .json({
+            user: { _id, name, email, role },
+            token,
+          });
+      })();
     });
-  };
+  }
 }
 
 export default new UserController();
